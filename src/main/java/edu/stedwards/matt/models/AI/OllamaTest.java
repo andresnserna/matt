fe41e25fa3a807fec9aa8c2afc0c60dae254f4c7
javierzavala2021 <javier.zavala2021@gmail.com>
2026-09-24 17:18:23 -0500

    fixing a syntax warning that was bothering me

--- src/main/java/edu/stedwards/matt/models/AI/OllamaTest.java
+++ src/main/java/edu/stedwards/matt/models/AI/OllamaTest.java
@@ -3,7 +3,7 @@ package edu.stedwards.matt.models.AI;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
+
 
 public class OllamaTest {
     public static void main(String[] args) {
